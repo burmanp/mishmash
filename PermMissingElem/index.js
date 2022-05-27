a376c36c4c09dfc54a69330bbcf1f5e23ff45a7f
burmanp <kohein@gmail.com>
2022-05-27 12:09:04 +0100

    tapeEquilibrium added

--- PermMissingElem/index.js
+++ PermMissingElem/index.js
@@ -30,16 +30,42 @@ each element of array A is an integer within the range [1..(N + 1)].
 [2,3,1,5]
 sort it first with a-b ascending => [1,2,3,5]
 loop through arr and if A[i] !== A[i+1]-1 => A[i+1]-1 
+
+//another algo
+loop and sum from 1 to max in array [2,3,1,5] - (1+2+3+4+5) => 15
+loop and add nums in array (1+2+3+5) => 11
+15 - 11 = 4
 */
 
 function solution(A) {
   A = A.sort((a, b) => a - b);
-  let missing;
+
   for (let i = 0; i < A.length; i++) {
     if (A[i] !== A[i + 1] - 1) {
-      return missing = A[i + 1] - 1;
+      return A[i + 1] - 1;
     }
   }
 }
 
-console.log(solution([4,3,1,7,6,5]));
+function solution2(A) {
+  let totalSum = 0;
+  let arraySum = A.reduce((pv, cv) => pv + cv, 0);
+  for (let i = 0; i <= Math.max(...A); i++) {
+    totalSum += i;
+  }
+  return totalSum - arraySum;
+}
+
+// console.time("solution1");
+// console.log(solution([4,3,1,7,6,5]));
+// console.timeEnd("solution1");
+// console.log("------");
+
+// console.time("solution2");
+// console.log(solution2([4,3,1,7,6,5]));
+// console.timeEnd("solution2");
+// console.log("-------");
+
+console.time("solution1");
+console.log(solution([4, 3, 6, 5]));
+console.timeEnd("solution1");
